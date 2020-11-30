fb28e5692bc9af067c8f472ede7694e84dc6b789
admin <admin@DESKTOP-47JDCQM>
2020-11-30 19:51:12 +0100

    un  constructor por defecto  que cree un alfil negro en la posición '8f'

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -4,6 +4,18 @@ public class Alfil {
 	private Color color;
 	private Posicion posicion;
 	
+	public Alfil(Color color) {
+		this.color=color;
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}
+		if (color==Color.BLANCO) {
+			posicion=new Posicion(1,'f');
+		}if (color==Color.NEGRO) {
+			this.posicion=new Posicion(8,'f');
+		}
+	}
+	
 	
 	public Color getColor() {
 		return color;
