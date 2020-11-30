140bf05355d37ce0da31102d90a2cd324c951acc
admin <admin@DESKTOP-47JDCQM>
2020-11-30 19:48:04 +0100

    Creo los métodos get y set  con la visibilidad adecuada

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -3,4 +3,24 @@ package org.iesalandalus.programacion.alfilajedrez;
 public class Alfil {
 	private Color color;
 	private Posicion posicion;
+	
+	
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+
 }
+
+
