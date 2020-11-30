c6322909aba5ffd31438c2840b876068b8331512
admin <admin@DESKTOP-47JDCQM>
2020-11-30 20:04:47 +0100

    Creo los métodos equals y hashCode

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
+import java.util.Objects;
+
 import javax.naming.OperationNotSupportedException;
 
 public class Alfil {
@@ -75,13 +77,24 @@ public class Alfil {
 				}
 				break;
 				
-
 		}
 
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(color, posicion);
+	}
 
-	
-
-		
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alfil other = (Alfil) obj;
+		return color == other.color && Objects.equals(posicion, other.posicion);
 	}
 
 	
