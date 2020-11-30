ea3b34e1e6f11530adfb59611e51c92d18355a2b
admin <admin@DESKTOP-47JDCQM>
2020-11-30 19:29:35 +0100

    Creo los métodos get y set

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -3,5 +3,18 @@ package org.iesalandalus.programacion.alfilajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+	
+	public int getFila() {
+		return fila;
+	}
+	 void setFila(int fila) {
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	 void setColumna(char i) {
+		this.columna = i;
+	}
 
 }
